760b16db45f5eaeb374c62c358d616fe80e66762
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2024-02-06 09:40:59 -0300

    altera coluna descricao fonte stn para caixa alta

--- scripts/fonte_stn.R
+++ scripts/fonte_stn.R
@@ -3,4 +3,5 @@ library(yaml)
 
 fonte_stn <- yaml.load_file("data-raw/fonte_stn.yaml") |> rbindlist()
 names(fonte_stn) <- toupper(names(fonte_stn))
+fonte_stn[, FONTE_STN_DESCRICAO := toupper(FONTE_STN_DESCRICAO)]
 readr::write_excel_csv2(fonte_stn, "data/fonte_stn.csv", quote = "needed")
